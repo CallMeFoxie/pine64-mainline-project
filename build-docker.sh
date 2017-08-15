e987a3cf17f450031a6f6ec5b5dc22ed074b97c3
Ashley <github@victorianfox.com>
2017-08-15 17:54:39 +0200

    Add Docker build script

---
+++ build-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION="v17.07.0-ce-rc2"
+
+echo "This has to be run on the PINE itself! Cross compiling is crazy annoying apparently..."
+
+pushd docker
+git clone https://github.com/docker/docker-ce.git --depth 1 --single-branch -b ${VERSION}
+cp config/docker-Dockerfile.aarch64 docker/docker-ce/components/packaging/deb/debian-stretch/Dockerfile.aarch64
+pushd docker-ce/components/packaging/deb
+make debian-stretch
+popd
+
+cp docker-ce/components/packaging/deb/debbuild/debian-stretch/docker-ce_*_arm64.deb ../output/output
+popd
